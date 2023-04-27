b71178ad5a6e6a744423b63a7ac001d5a995fbef
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-04-27 13:12:16 +0200

    feat: added ontology annotate element

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,61 @@
 <script setup lang="ts">
-import OntologyAnnotation from "./components/ontology-annotation.ce.vue";
-import OntologyAutoComplete from "./components/ontology-autocomplete.ce.vue";
-// import OntologyCompose from "./components/ontology-compose.ce.vue";
-import OntologyAnnotate from "./components/ontology-annotate.ce.vue";
-import { ref } from "vue";
-let selectedValue = ref(
-  "Homo sapiens	OBI	http://purl.obolibrary.org/obo/NCBITaxon_9606	class"
-);
+import OntologyAnnotation from './components/ontology-annotation.ce.vue'
+import OntologyAutoComplete from './components/ontology-autocomplete.ce.vue'
+import OntologyCompose from './components/ontology-compose.ce.vue'
+import OntologyAnnotate from './components/ontology-annotate.ce.vue'
+import { ref } from 'vue'
+let autoCompleteValue = ref('')
+let selectedValue = ref('Homo sapiens	OBI	http://purl.obolibrary.org/obo/NCBITaxon_9606	class')
+let annotatedContent = ref('')
+let composedContent = ref('')
 </script>
 <template>
   <header>
     <div class="wrapper">
+      <h2>Ontology Annotation</h2>
       <ontology-annotation :annotation="selectedValue"></ontology-annotation>
-      <br />
-      <br />
+      <hr />
+      <h2>Ontology Autocomplete / Type ahead</h2>
       <ontology-auto-complete
-        :modelValue="selectedValue"
-        @change="selectedValue = $event"
+        @change="autoCompleteValue = $event"
         format="text"
         label="Input field (ontology driven)"
         info="Search terms across ontologies"
         placeholder="search term"
       ></ontology-auto-complete>
       <br />
-      <code v-if="selectedValue">
+      <code v-if="autoCompleteValue">
         Value:
-        <pre>{{ selectedValue }}</pre>
+        <pre>{{ autoCompleteValue }}</pre>
       </code>
       <hr />
+      <h2>Ontology Annotate</h2>
       <ontology-annotate
         format="text"
         label="Input field (ontology driven)"
         info="Search terms across ontologies"
+        @change="annotatedContent = $event"
       ></ontology-annotate>
+      <div>
+        <code>
+          <pre>{{ annotatedContent }}</pre>
+        </code>
+      </div>
+      <hr />
+      <div>
+        <h2>Ontology Compose</h2>
+        <ontology-compose
+          format="text"
+          label="Input field (ontology driven)"
+          info="Search terms across ontologies"
+          @change="composedContent = $event"
+        ></ontology-compose>
+        <div>
+          <code>
+            <pre>{{ annotatedContent }}</pre>
+          </code>
+        </div>
+      </div>
     </div>
   </header>
 </template>
--- src/components/ontology-annotate.ce.vue
+++ src/components/ontology-annotate.ce.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <label v-if="label">{{ label }}</label>
     <div>
       <div
         @mouseup="checkSelection"
@@ -12,141 +13,228 @@
         {{ content }}
       </div>
     </div>
+    <div>
+      <div v-if="loading" style="float: right">
+        <i>...loading</i>
+      </div>
+      <div>
+        <span v-if="info">{{ info }}</span>
+      </div>
+    </div>
     <div class="contextmenu" :style="'left:' + cords.x + 'px; top:' + cords.y + 'px'">
-      <div
-        class="contextmenu-item"
-        v-for="doc in matches"
-        :key="doc['short_label']"
-        role="option"
-        tabindex="-1"
-        aria-selected="false"
-        aria-setsize="3"
-        aria-posinset="0"
-        @click="selectTerm(doc)"
-      >
-        <p v-html="highlight(doc['label'])"></p>
-        <small class="iri">{{ doc["ontology_prefix"] }}:{{ doc["iri"] }}</small>
+      <div class="contextmenu-item" v-if="matches.length == 0 && !loading">No matches found</div>
+      <div v-else>
+        <div
+          class="contextmenu-item"
+          v-for="doc in matches"
+          :key="doc['short_label']"
+          role="option"
+          tabindex="-1"
+          aria-selected="false"
+          aria-setsize="3"
+          aria-posinset="0"
+          @click.capture.stop.prevent="selectTerm(doc)"
+        >
+          <p v-html="highlight(doc['label'])"></p>
+          <small class="iri">{{ doc['ontology_prefix'] }}:{{ doc['iri'] }}</small>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed, reactive } from 'vue'
+
+const emit = defineEmits(['change'])
 
 const props = defineProps({
   label: {
     type: String,
     required: false,
-    default: "",
+    default: ''
   },
   info: {
     type: String,
     required: false,
-    default: "",
+    default: ''
   },
   placeholder: {
     type: String,
     required: false,
-    default: "",
+    default: ''
   },
   ontologies: {
     type: String,
     required: false,
-    default: "",
+    default: ''
   },
   format: {
     type: String,
     required: false,
-    default: "text",
-  },
-});
+    default: 'text'
+  }
+})
 
-let content = ref(
-  "The lichen natural products pulvinamide, rhizocarpic acid, and epanorin have been synthesized and characterized spectroscopically and by X-ray crystallography. The syntheses, by ring-opening of pulvinic acid dilactone (PAD), may well be biomimetic, given the well-known occurrence of PAD in lichen. The enantiomers, ent-rhizocarpic acid and ent-epanorin, and corresponding carboxylic acids, norrhizocarpic acid and norepanorin, were similarly prepared. All compounds were assessed for growth inhibitory activity against selected bacteria, fungi, a protist, a mammalian tumor cell line, and normal cells. Rhizocarpic acid is weakly antibacterial (Bacillus subtilis MIC = 50 μg/mL) and possesses modest but selective antitumor activity (NS-1 murine myeloma MIC = 3.1 μg/mL) with >10-fold potency relative to its enantiomer (MIC = 50 μg/mL)."
-);
+let content = ref('')
 
-let searchTerm = ref("");
+const bratOnto = computed(() => {
+  if (!content.value) {
+    return null
+  }
+  if (props.format && props.format == 'json') {
+    return {
+      text: content.value,
+      ontology: selectedTerms
+    }
+  } else {
+    return (
+      content.value +
+      '\n$$$$\n' +
+      selectedTerms
+        .map(
+          (t) =>
+            t.ontology['type'][0] +
+            '\t' +
+            t.ontology['ontology_prefix'] +
+            ' ' +
+            t.context['start'] +
+            ' ' +
+            t.context['end'] +
+            '\t' +
+            t.ontology['label'] +
+            '\t' +
+            t.ontology['iri']
+        )
+        .join('\n')
+    )
+  }
+})
 
-let matches = ref([]);
+let searchTerm = ref('')
 
-let cords: any = ref({ x: -1000, y: 0, start: -1, end: -1 });
+let loading = ref(false)
 
-function onInput(e: any) {
-  content.value = e.target.innerText;
+let matches = ref([])
+
+type MyType = {
+  ontology: any
+  context: any
 }
 
-function selectTerm() {}
+let selectedTerms: Array<MyType> = reactive([])
 
-function checkSelection() {
+const pendingRequests = new WeakMap()
+
+let cords: any = ref({ x: -1000, y: 0, start: -1, end: -1 })
+
+function onInput(e: any) {
+  content.value = e.target.innerText
+  emit('change', bratOnto.value)
+}
+
+function selectTerm(doc: any) {
+  let data = { ontology: doc, context: cords.value }
+  selectedTerms.push(data)
+  cords.value = { x: -1000, y: 0, start: -1, end: -1 }
+  matches.value = []
+  searchTerm.value = ''
+  emit('change', bratOnto.value)
+  // var selectionRange = window.getSelection()?.getRangeAt(0);
+  // var selectedText = selectionRange?.extractContents();
+  // var span = document.createElement("span");
+  // span.style.backgroundColor = "yellow";
+  // span.appendChild(selectedText!);
+  // selectionRange?.insertNode(span);
+}
+
+// function onBlur() {
+//   searchTerm.value = ''
+//   matches.value = []
+//   cords.value = { x: -1000, y: 0, start: -1, end: -1 }
+// }
+
+function checkSelection(e: any) {
+  searchTerm.value = ''
   if (window.getSelection) {
-    searchTerm.value = window.getSelection()?.toString()!;
-    var selection = window.getSelection()?.getRangeAt(0);
-    var selectedText = selection?.extractContents();
-    var span = document.createElement("span");
-    span.style.backgroundColor = "yellow";
-    span.appendChild(selectedText!);
-    selection?.insertNode(span);
-
-    if (searchTerm.value) {
-      getSelectOptions();
-      getSelectionCoords();
+    var selection = window.getSelection()
+    searchTerm.value = selection?.toString()!
+    if (searchTerm.value && searchTerm.value != '') {
+      matches.value = []
+      cords.value = { x: -1000, y: 0, start: -1, end: -1 }
+      getSelectionCoords(selection)
+      getSelectOptions(e)
     } else {
-      cords.value = { x: -1000, y: 0 };
+      cords.value = { x: -1000, y: 0, start: -1, end: -1 }
+      matches.value = []
     }
   }
 }
 
 function highlight(content: string) {
   if (!searchTerm.value) {
-    return content;
+    return content
   }
-  return content.replace(new RegExp(searchTerm.value, "gi"), (match) => {
-    return '<span class="highlightText">' + match + "</span>";
-  });
+  return content.replace(new RegExp(searchTerm.value, 'gi'), (match) => {
+    return '<span class="highlightText">' + match + '</span>'
+  })
 }
 
-function getSelectionCoords(atStart: boolean = true) {
-  const sel = window.getSelection();
-  if (!sel?.rangeCount) return null;
-  let range = sel.getRangeAt(0).cloneRange();
-  if (!range.getClientRects) return null;
-  range.collapse(atStart);
-  let rects = range.getClientRects();
-  if (rects.length <= 0) return null;
-  let rect = rects[0];
-  cords.value.start = sel.anchorOffset;
-  cords.value.end = sel.focusOffset;
-  cords.value.x = rect.x;
-  cords.value.y = rect.y + rects[0]?.height;
+function getSelectionCoords(sel: any, atStart: boolean = true) {
+  if (!sel?.rangeCount) return null
+  let range = sel.getRangeAt(0).cloneRange()
+  if (!range.getClientRects) return null
+  range.collapse(atStart)
+  let rects = range.getClientRects()
+  if (rects.length <= 0) return null
+  let rect = rects[0]
+  cords.value.start = sel.anchorOffset
+  cords.value.end = sel.focusOffset
+  cords.value.x = rect.x
+  cords.value.y = rect.y + rects[0]?.height
 }
 
-async function getSelectOptions() {
-  if (searchTerm.value === "") {
-    matches.value = [];
-    return [];
+async function getSelectOptions(event: any) {
+  if (searchTerm.value === '') {
+    matches.value = []
+    return []
   }
-  let ontologiesExists = searchTerm.value.indexOf(":");
-  let ontologies = "";
-  let term = null;
+  let ontologiesExists = searchTerm.value.indexOf(':')
+  let ontologies = ''
+  let term = null
   if (ontologiesExists < 0) {
-    ontologies = props.ontologies;
-    term = searchTerm.value;
+    ontologies = props.ontologies
+    term = searchTerm.value
   } else {
-    term = searchTerm.value.split(":")[1];
-    ontologies = searchTerm.value.split(":")[0];
+    term = searchTerm.value.split(':')[1]
+    ontologies = searchTerm.value.split(':')[0]
   }
 
-  const url = "https://service.tib.eu/ts4tib/api/select?q=";
-  const queries = encodeURI(
-    "&ontology=" +
-      ontologies +
-      "&fieldList=iri,label,short_form,obo_id,ontology_name,ontology_prefix,description,type&obsoletes=false&local=false&rows=10"
-  );
+  const ele = event.currentTarget
+  const previousController = pendingRequests.get(ele)
 
-  let response = (await fetch(`${url}${term}${queries}`)).json();
+  if (previousController) {
+    previousController.abort()
+  }
+
+  const controller = new AbortController()
+  pendingRequests.set(ele, controller)
+
+  const url = 'https://service.tib.eu/ts4tib/api/select?q='
+  const queries = encodeURI(
+    '&ontology=' +
+      ontologies +
+      '&fieldList=iri,label,short_form,obo_id,ontology_name,ontology_prefix,description,type&obsoletes=false&local=false&rows=10'
+  )
+
+  loading.value = true
+
+  let response = (await fetch(`${url}${term}${queries}`, { signal: controller.signal })).json()
+
   response.then((body) => {
-    matches.value = body.response.docs;
-  });
+    pendingRequests.delete(ele)
+    matches.value = body.response.docs
+    loading.value = false
+  })
 }
 </script>
 <style></style>
