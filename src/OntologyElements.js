ae924cb80d2d08bd12967a8d7d3ad96bb0b230e1
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-05-16 22:32:26 +0200

    feat: exposed annotate and compose elements

--- src/OntologyElements.js
+++ src/OntologyElements.js
@@ -1,9 +1,15 @@
 import { defineCustomElement } from 'vue'
 import VueOntologyAnnotation from './components/ontology-annotation.ce.vue'
 import VueOntologyAutoComplete from './components/ontology-autocomplete.ce.vue'
+import VueOntologyAnnotate from './components/ontology-annotate.ce.vue'
+import VueOntologyCompose from './components/ontology-compose.ce.vue'
 
 export const OntologyAnnotation = defineCustomElement(VueOntologyAnnotation)
 export const OntologyAutoComplete = defineCustomElement(VueOntologyAutoComplete)
+export const OntologyAnnotate = defineCustomElement(VueOntologyAnnotate)
+export const OntologyCompose = defineCustomElement(VueOntologyCompose)
 
 customElements.define('ontology-annotation', OntologyAnnotation)
 customElements.define('ontology-autocomplete', OntologyAutoComplete)
+customElements.define('ontology-annotate', OntologyAnnotate)
+customElements.define('ontology-compose', OntologyCompose)
