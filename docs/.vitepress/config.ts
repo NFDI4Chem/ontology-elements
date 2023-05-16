1c8ad71b7f196fa6874f1694a5ef2dda96b34a56
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-05-17 01:24:09 +0200

    feat: added docs site

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "ontology-elements",
+  description: "ontology-elements docs",
+  base: '/NFDI4Chem/ontology-elements/',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Docs', link: '/docs' },
+      { text: 'Demo', link: '/demo' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Introduction',
+        items: [
+          { text: 'What are ontology elements?', link: '/markdown-examples' },
+          { text: 'Getting started', link: '/api-examples' },
+          { text: 'Ontology Lookup Service (OLS)', link: '/api-examples' },
+        ]
+      },
+      {
+        text: 'Components',
+        items: [
+          { text: 'Annotation', link: '/markdown-examples' },
+          { text: 'Autocomplete (Input)', link: '/api-examples' },
+          { text: 'Compose (Textarea)', link: '/api-examples' },
+          { text: 'Annotate (Textarea)', link: '/api-examples' },
+        ]
+      },
+      {
+        text: 'Usage',
+        items: [
+          { text: 'React', link: '/markdown-examples' },
+          { text: 'Angular', link: '/api-examples' },
+          { text: 'Vuejs', link: '/api-examples' },
+          { text: 'No framework', link: '/api-examples' },
+        ]
+      },
+      {
+        text: 'Development',
+        items: [
+          { text: 'Local setup', link: '/markdown-examples' },
+          { text: 'FAQs', link: '/api-examples' },
+          { text: 'License', link: '/api-examples' },
+          { text: 'Issues / Feature requests', link: '/api-examples' },
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/NFDI4Chem/ontology-elements' }
+    ],
+
+    footer: {
+      message: 'Licensed under the <a href="https://github.com/NFDI4Chem/ontology-elements/blob/main/LICENSE">MIT License</a>.',
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+layout: home
+
+hero:
+  name: "Ontology Elements"
+  text: "Enabling data interoperability across web"
+  tagline: "Custom rich ontology driven html elements (web components) that can be used with any web framework, or even without a framework."
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Semantic data capture
+    details: Ontology-driven web components ensure a consistent and standardized representation of data, facilitating seamless integration and interpretation of information within life sciences web applications
+  - title: Ontology-driven BRAT standoff output
+    details: OE combines the benefits of ontologies with the flexibility of the BRAT standoff format for annotating text.
+  - title: Annotation without database schema modifications
+    details: Ontology-driven BRAT standoff format allows for annotating text without requiring any changes to the underlying database schema
+---
+
